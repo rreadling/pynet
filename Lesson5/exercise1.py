8f865f8c1791b1c2b10ad1233f5b8e705ea7808a
rreadling <rob.readling@gmail.com>
2020-07-11 12:27:57 -0400

    framework complete

--- Lesson5/exercise1.py
+++ Lesson5/exercise1.py
@@ -6,6 +6,12 @@ Call this ssh_conn function using entirely positional arguments.
 Call this ssh_conn function using entirely named arguments.
 
 Call this ssh_conn function using a mix of positional and named arguments.
+
+1b. Expand on the ssh_conn function from exercise1 except add a fourth parameter 'device_type' with a default value of 'cisco_ios'. Print all four of the function variables out as part of the function's execution.
+
+Call the 'ssh_conn2' function both with and without specifying the device_type
+
+Create a dictionary that maps to the function's parameters. Call this ssh_conn2 function using the **kwargs technique.
 """
 
 def ssh_conn(ip_addr, username, password):
---
+++ Lesson5/exercise4.py
@@ -0,0 +1,12 @@
+"""
+4. Copy your solution from exercise3 to exercise4. Add an 'import pdb' and pdb.set_trace() statement outside of your function (i.e. where you have your function calls).
+
+Inside of pdb, experiment with:
+
+    Listing your code.
+    Using 'next' and 'step' to walk through your code. Make sure you understand the difference between next and step.
+    Experiment with 'up' and 'down' to move up and down the code stack.
+    Use p <variable> to inspect a variable.
+    Set a breakpoint and run your code to the breakpoint.
+    Use '!command' to change a variable (for example !new_mac = [])
+"""
